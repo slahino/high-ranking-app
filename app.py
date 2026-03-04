d1c8c5b6c937009f240cf0afb4c6658f18ce7964
slahino <73803271+slahino@users.noreply.github.com>
2026-03-04 03:25:45 +0100

    check app.py

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
 from flask import Flask
-
+import os
 
 from app.routes.auth import auth_bp
 from app.routes.vote import vote_bp
 from app.routes.resultat import resultat_bp
 
-
 app = Flask(__name__, template_folder="app/templates")
 
-import os
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(vote_bp)
 app.register_blueprint(resultat_bp)
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
 @app.route("/healthz")
 def health():
     return "OK", 200
 
 
+if __name__ == "__main__":
+    app.run(debug=True)
