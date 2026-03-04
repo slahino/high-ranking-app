89f28f7a5e0be32704c80104d61c07fc6a69b83c
slahino <73803271+slahino@users.noreply.github.com>
2026-03-04 06:19:25 +0100

    change sender

--- app/services/send_email.py
+++ app/services/send_email.py
@@ -10,7 +10,7 @@ def send_email(to_email, token):
     payload = {
         "sender": {
             "name": "Vote Condorcet",
-            "email": "vote@condorcet.fr"
+            "email": "souaa.yassine@gmail.com"
         },
         "to": [
             {"email": to_email}
