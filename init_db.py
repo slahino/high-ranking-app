aa7863762ab15aacc55a8e601897e346401be056
slahino <73803271+slahino@users.noreply.github.com>
2026-03-04 16:24:02 +0100

    add table users

--- init_db.py
+++ init_db.py
@@ -14,7 +14,7 @@ def init_db():
       a_vote BOOLEAN DEFAULT 0
   )
   """)
-  
+
   cursor.execute("""
   CREATE TABLE IF NOT EXISTS tokens (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -24,7 +24,7 @@ def init_db():
       actif BOOLEAN DEFAULT 1
   )
   """)
-  
+
   cursor.execute("""
   CREATE TABLE IF NOT EXISTS projets (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -35,28 +35,10 @@ def init_db():
   )
   """)
   
-  cursor.execute("""
-  INSERT INTO utilisateurs (id, nom, prenom, email, role, a_vote)
-  VALUES (1, 'SOUA', 'YASSINE','yassine.soua@ac-nancy-metz.fr','Professeur',false)
-  """)
   
-  cursor.execute("""
-  INSERT INTO projets (id, nom, image, description, nb_votes)
-  VALUES (1, 'Projet NASDAS', 'https://hoteldorleans.fr/media/le-belvedere-vertiges-des-cimes.jpg','Belveno: numero 1 en France et à forbach',0)
-  """)
-  
-  cursor.execute("""
-  INSERT INTO projets (id, nom, image, description, nb_votes)
-  VALUES (2, 'Projet MONSTRE', 'https://hoteldorleans.fr/media/le-belvedere-vertiges-des-cimes.jpg','Belveno: numero 1 en France et à forbach',0)
-  """)
-  
-  cursor.execute("""
-  INSERT INTO projets (id, nom, image, description, nb_votes)
-  VALUES (3, 'Projet ENCULO', 'https://hoteldorleans.fr/media/le-belvedere-vertiges-des-cimes.jpg','Belveno: numero 1 en France et à forbach',0)
-  """)
-  
+
+ 
   conn.commit()
   conn.close()
-  
+
   print("Base initialisée avec succès.")
-  
--- init_postgres.py
+++ init_postgres.py
@@ -1,7 +1,11 @@
 import psycopg2
+import csv
 import os 
 
-conn = psycopg2.connect("postgresql://vote_condorcet_db_user:ofHxBK0dFwRm0zBZ0GdmYE579FiYzsEj@dpg-d6jvtacr85hc73br6clg-a.oregon-postgres.render.com/vote_condorcet_db")
+#DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = "postgresql://vote_condorcet_db_user:ofHxBK0dFwRm0zBZ0GdmYE579FiYzsEj@dpg-d6jvtacr85hc73br6clg-a.oregon-postgres.render.com/vote_condorcet_db"
+
+conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
 
 cursor.execute("""
@@ -14,6 +18,27 @@ cursor.execute("""
       a_vote BOOLEAN DEFAULT FALSE
   )
   """)
+
+cursor.execute("DELETE FROM Utilisateurs")
+
+with open("professeurs.csv", newline="", encoding="utf-8-sig") as file:
+  reader = csv.DictReader(file, delimiter=";")
+  
+  for row in reader:
+    cursor.execute("""
+    INSERT INTO utilisateurs (id, nom, prenom, email, role, a_vote)
+    VALUES (%s,%s,%s,%s,%s,%s)
+      """, (
+        row["id"],
+        row["nom"],
+        row["prenom"],
+        row["email"],
+        row["role"],
+        False
+      ))
+
+  
+print("Table utilisteurs initialisée avec succès.")
   
 cursor.execute("""
   CREATE TABLE IF NOT EXISTS tokens (
