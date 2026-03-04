23c735e65011060c64b113934eb3479482b2e13c
slahino <73803271+slahino@users.noreply.github.com>
2026-03-04 06:12:24 +0100

    brevo new techno

--- app/services/send_email.py
+++ app/services/send_email.py
@@ -1,19 +1,32 @@
-import resend
+import requests
 import os
-
-resend.api_key = os.environ["RESEND_API_KEY"]
 
 def send_email(to_email, token):
 
+    url = "https://api.brevo.com/v3/smtp/email"
+
     link = f"{os.environ.get('BASE_URL')}/vote/{token}"
 
-    resend.Emails.send({
-        "from": "Vote App <onboarding@resend.dev>",
-        "to": [to_email],
-        "subject": "Votre lien de vote",
-        "html": f"""
-        <h2>Lien de vote</h2>
-        <p>Cliquez ici pour voter :</p>
+    payload = {
+        "sender": {
+            "name": "Vote Condorcet",
+            "email": "vote@condorcet.fr"
+        },
+        "to": [
+            {"email": to_email}
+        ],
+        "subject": "Lien pour voter",
+        "htmlContent": f"""
+        <h2>Vote du lycée Condorcet</h2>
+        <p>Cliquez sur le lien ci-dessous pour voter :</p>
         <a href="{link}">{link}</a>
         """
-    })
+    }
+
+    headers = {
+        "accept": "application/json",
+        "api-key": os.environ.get("BREVO_API_KEY"),
+        "content-type": "application/json"
+    }
+
+    requests.post(url, json=payload, headers=headers)
--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 
 VOTE_END_DATE = datetime(2026, 3, 25, 18, 0, 0)
 
-BASE_URL = os.environ.get("BASE_URL")
+BASE_URL = "https://vote-condorcet-belevedere.onrender.com"
 RESEND_API_KEY = os.environ.get("RESEND_API_KEY")
