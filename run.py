11772e8d1d432a764d01d4233bc20eaa67afd19e
slahino <73803271+slahino@users.noreply.github.com>
2026-03-04 03:49:10 +0100

    add bdd render

--- run.py
+++ run.py
@@ -4,10 +4,13 @@ import os
 from app.routes.auth import auth_bp
 from app.routes.vote import vote_bp
 from app.routes.resultat import resultat_bp
+from init_db import init_db
 
 app = Flask(__name__, template_folder="app/templates")
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+init_db()
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(vote_bp)
