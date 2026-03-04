eadb162c8f38eda55a0b405b34fe0922f684b3cf
slahino <73803271+slahino@users.noreply.github.com>
2026-03-04 05:16:19 +0100

    modif mdp email et base

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from datetime import datetime
 
 VOTE_END_DATE = datetime(2026, 3, 25, 18, 0, 0)
 
-BASE_URL = "http://127.0.0.1:5000"
+BASE_URL = "https://vote-condorcet-belevedere.onrender.com/"
 EMAIL_SENDER = "souaa.yassine@gmail.com"
 EMAIL_PASSWORD = "liuysllrknoknbxm"
