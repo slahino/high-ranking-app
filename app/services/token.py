db9305ca874a83c431e59a5350615db29f8d448c
slahino <73803271+slahino@users.noreply.github.com>
2026-03-04 16:52:41 +0100

    check postgres to sqlite3

--- app/services/token.py
+++ app/services/token.py
@@ -1,5 +1,10 @@
 import secrets
-import sqlite3
+import psycopg2
+import os 
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+
 
 def generate_token():
     return secrets.token_urlsafe(32)
@@ -7,13 +12,13 @@ def generate_token():
   
 
 def invalidate_tokens(user_id):
-    conn = sqlite3.connect("database.db", timeout=10)
-    cursor = conn.cursor()
+  conn = psycopg2.connect(DATABASE_URL,timeout=10)
+  cursor = conn.cursor()
+  
+  cursor.execute(
+      "DELETE FROM tokens WHERE utilisateur_id = %s",
+      (user_id,)
+  )
 
-    cursor.execute(
-        "DELETE FROM tokens WHERE utilisateur_id = %s",
-        (user_id,)
-    )
-
-    conn.commit()
-    conn.close()
+  conn.commit()
+  conn.close()
